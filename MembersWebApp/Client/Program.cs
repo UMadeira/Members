d64cc4446d1f115f8637f908d56ca5958890af38
Leonel Nobrega <lnobrega@uma.pt>
2023-04-26 10:36:24 +0100

    Authentication

--- MembersWebApp/Client/Program.cs
+++ MembersWebApp/Client/Program.cs
@@ -12,14 +12,20 @@ namespace MembersWebApp
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { 
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddOidcAuthentication(options =>
-            {
-                // Configure your authentication provider options here.
-                // For more information, see https://aka.ms/blazor-standalone-auth
-                builder.Configuration.Bind( "Local", options.ProviderOptions );
+            //builder.Services.AddOidcAuthentication(options =>
+            //{
+            //    // Configure your authentication provider options here.
+            //    // For more information, see https://aka.ms/blazor-standalone-auth
+            //    builder.Configuration.Bind("Local", options.ProviderOptions);
+            //});
+
+            builder.Services.AddMsalAuthentication( options => {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
             });
+
 
             await builder.Build().RunAsync();
         }
